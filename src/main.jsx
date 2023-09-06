5ac7c1dfc2daabf59bebd695848d3310948ea947
GEdO23 <rm99632@fiap.com.br>
2023-09-06 09:33:16 -0300

    Criação das rotas Home e Error, e roteação

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App from './App.jsx'
 import './index.css'
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
+import Error from './routes/Error/index.jsx'
+import Home from './routes/Home/index.jsx'
 
 const router = createBrowserRouter(
   [
-    {path: '/', element: <App/>}
+    {path: '/', element: <App/>, 
+    errorElement: <Error/>, 
+    children: [
+      { path: '/', element: <Home />}
+    ]}
   ]
 )
 
---
+++ src/routes/Error/index.jsx
@@ -0,0 +1,11 @@
+
+export default function Error() {
+
+  return (
+    <>
+    
+        <div>Erro - 404</div>
+
+    </>
+  )
+}
