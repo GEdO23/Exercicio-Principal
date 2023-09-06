da31af575340970cd581a0b82b094770a4c47120
GEdO23 <rm99632@fiap.com.br>
2023-09-06 10:02:19 -0300

    Add forms a pagina de Editar Produtos

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -1,15 +1,33 @@
+// import { useParams } from "react-router-dom";
 
 export default function EditarProdutos() {
 
-  return (
-    <>
+    document.title = "Editar Produtos"
 
-        <div>
+    //Recuperando o id do produto com o HOOK useParam();
+    // const {id} = useParams();
 
-            <h1>EDITOR MODE</h1>
-        
-        </div>
+    return (<>
+    <div>
+        <h1>EDITAR PRODUTOS</h1>
+        <form action="#" method="get">
+            <fieldset>
+                <legend>EDITAR PRODUTO</legend>
 
-    </>
-  )
+                <div>
+                    <label htmlFor="idNmProduto">Nome: </label>
+                    <input type="text" name="nome" id="idNmProduto"/>
+                </div>
+                <div>
+                    <label htmlFor="idVlProduto">Preço: </label>
+                    <input type="text" name="valor" id="idVlProduto"/>
+                </div>
+
+                <div>
+                    <button>EDITAR</button>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+    </>)
 }
